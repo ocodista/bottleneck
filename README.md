fe7e4c2a9753f9e15ce64e8e7d7c80bfdeb5be21
Caio Borghi <caiohoborghi@gmail.com>
2023-09-30 16:07:14 -0300

    Improve README.md

--- README.md
+++ README.md
@@ -1,15 +1,44 @@
-# Elysia with Bun runtime
+# Measurer.js
 
-## Getting Started
-To get started with this template, simply paste this command into your terminal:
-```bash
-bun create elysia ./elysia-example
+Measurer.js is a CLI tool that profiles all JavaScript functions of an input file and generates a report file. It's built with Node.js and uses the Bun runtime for fast execution.
+
+![Banner](banner.png)
+
+## Features
+
+- Measures the execution time of all functions in a JavaScript file
+- Supports both Node.js and Bun runtimes
+- Outputs a report file with the profiling results
+
+## Installation
+
+You can install Measurer.js globally with npm:
+```
+bash
+npm install -g measurer.js
 ```
 
-## Development
-To start the development server run:
-```bash
-bun run dev
+## Usage
+
+To measure the execution time of all functions in a file, use the `measure` command:
+```
+measurer.js measure <filePath>
 ```
 
-Open http://localhost:3000/ with your browser to see the result.
+You can specify the output file path with the `-o` or `--output` option:
+```
+measurer.js measure <filePath> -o <outputPath>
+```
+
+You can specify the runtime to use with the `-r` or `--runtime` option. The default is `bun`, but you can also use `node`:
+```
+measurer.js measure <filePath> -r node
+```
+
+## Contributing
+
+We welcome contributions to Measurer.js! Please see our [CONTRIBUTING.md](CONTRIBUTING.md) for more details.
+
+## License
+
+Measurer.js is licensed under the [MIT License](LICENSE).
--- src/cli.js
+++ src/cli.js
@@ -1,27 +1,57 @@
 import { program } from 'commander';
 import chalk from 'chalk';
 import { spawn } from 'bun';
+import figlet from 'figlet';
 
-program
-  .name('measurer.js')
-  .version('1.0.0')
-  .description(chalk.green('measurer.js profiles all JavaScript functions of an input file and generates a report file.'));
+const DEFAULT_OUTPUT_PATH = 'output.txt';
+const DEFAULT_RUNTIME = 'bun';
+const NODE_RUNTIME = 'node';
+const NODE_SCRIPT_PATH = './src/node-esm-measurer.js';
+const BUN_SCRIPT_PATH = './src/bun-measurer.js';
 
-program
-  .command('measure <filePath>')
-  .description('Measures the execution time of all functions')
-  .option('-o, --output <outputPath>', 'Output file path', 'output.txt')
-  .option('-r, --runtime <runtime>', 'Runtime to use', 'bun')
-  .action(async (filePath, options) => {
-    const { output, runtime } = options;
-    const scriptPath = runtime === 'node' ? './src/node-esm-measurer.js' : './src/bun-measurer.js';
-    const proc = spawn([runtime, scriptPath, filePath, output], {
-      onExit: (_proc, exitCode) => {
-        exitCode && console.log(chalk.red(`Process exited with code ${exitCode}`));
-      }
-    });
-    const outputText = await new Response(proc.stdout).text()
-    console.log(outputText);
-  });
+function printBanner() {
+  console.log(
+    chalk.green(
+      figlet.textSync('measurer.js', {
+        horizontalLayout: 'full',
+      })
+    )
+  );
+}
 
-program.parse(process.argv);
+function handleExit(_proc, exitCode) {
+  if (exitCode) {
+    console.log(chalk.red(`Process exited with code ${exitCode}`));
+  }
+}
+
+async function measure(filePath, options) {
+  const { output = DEFAULT_OUTPUT_PATH, runtime = DEFAULT_RUNTIME } = options;
+  const scriptPath = runtime === NODE_RUNTIME ? NODE_SCRIPT_PATH : BUN_SCRIPT_PATH;
+  const proc = spawn([runtime, scriptPath, filePath, output], { onExit: handleExit });
+  const outputText = await new Response(proc.stdout).text();
+  console.log(outputText);
+}
+
+function setupProgram() {
+  program
+    .name('measurer.js')
+    .version('1.0.0')
+    .description('Profiles all JavaScript functions of an input file and generates a report file.');
+
+  program
+    .command('measure <filePath>')
+    .description('Measures the execution time of all functions')
+    .option('-o, --output <outputPath>', 'Output file path', DEFAULT_OUTPUT_PATH)
+    .option('-r, --runtime <runtime>', 'Runtime to use', DEFAULT_RUNTIME)
+    .action(measure);
+
+  program.parse(process.argv);
+}
+
+function main() {
+  printBanner();
+  setupProgram();
+}
+
+main();
